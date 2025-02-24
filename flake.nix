c002675161a4859bc25ce024bcf8e6b973beb6f7
APnda <apnda6661@proton.me>
2025-02-24 05:26:09 -0600

    use flake-utils

--- flake.nix
+++ flake.nix
@@ -3,23 +3,26 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }: 
+  outputs = { self, nixpkgs, flake-utils }: 
+  flake-utils.lib.eachDefaultSystem (system:
   let 
-    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    pkgs = nixpkgs.legacyPackages.${system};
   in
   {
 
-    packages.x86_64-linux.maomaowm = pkgs.stdenv.mkDerivation {
-      pname = "maomao";
-      version = "v0.1.5";
+    packages = rec {
+      maomaowm = pkgs.stdenv.mkDerivation {
+        pname = "maomao";
+        version = "v0.1.5";
 
-      src = ./.;
+        src = ./.;
 
-      patches = [
-        (pkgs.writeText "fix.diff" /* diff */
-         ''
+        patches = [
+          (pkgs.writeText "fix.diff" /* diff */
+           ''
 diff --git a/meson.build b/meson.build
 index eeec918..a73139b 100644
 --- a/meson.build
@@ -30,71 +33,72 @@ index eeec918..a73139b 100644
  install_data('maomao.desktop', install_dir : desktop_install_dir)
 -install_data('config.conf', install_dir : '/etc/maomao')
 +#install_data('config.conf', install_dir : 'etc/maomao')
-         ''
-        )
-      ];
+           ''
+          )
+        ];
 
-      nativeBuildInputs = with pkgs; [ 
-        meson
-        cmake
-        pkg-config
-        ninja
-      ];
+        nativeBuildInputs = with pkgs; [ 
+          meson
+          cmake
+          pkg-config
+          ninja
+        ];
 
-      buildInputs = with pkgs; [
-        wlroots_0_17
-        wayland
-        wayland-protocols 
-        wayland-scanner
-        xorg.libxcb.dev
-        xorg.xcbutilwm
-        libxkbcommon
-        libinput
-        pixman
-        xorg.libX11
-      ];
+        buildInputs = with pkgs; [
+          wlroots_0_17
+          wayland
+          wayland-protocols 
+          wayland-scanner
+          xorg.libxcb.dev
+          xorg.xcbutilwm
+          libxkbcommon
+          libinput
+          pixman
+          xorg.libX11
+        ];
 
+      };
+
+      default = maomaowm;
     };
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.maomaowm;
 
-    nixosModules.default = 
-    {config, lib, ...}: 
-    with lib; 
-    let 
-      cfg = config.programs.maomao;
-    in
-    {
-      options = {
-        programs.maomao = {
-          enable = mkEnableOption ''
-            maomaowm is a wayland compositor based on dwl(0.5) , adding many
-            operation that supported in hyprland and a hyprland-like keybinds,
-            niri-like scroll layout and sway-like scratchpad. See below for
-            more features.
-          '';
-
-          configFile = mkOption {
-            default = ./config.conf;
-            type = with types; nullOr path;
-            description = ''
-              The fallback configuration to use at /etc/maomao/config.conf
+    nixosModules = {
+      default = {config, lib, ...}: 
+        with lib; 
+      let 
+        cfg = config.programs.maomao;
+      in
+      {
+        options = {
+          programs.maomao = {
+            enable = mkEnableOption ''
+              maomaowm is a wayland compositor based on dwl(0.5) , adding many
+              operation that supported in hyprland and a hyprland-like
+              keybinds, niri-like scroll layout and sway-like scratchpad. See
+              below for more features.
               '';
+
+            configFile = mkOption {
+              default = ./config.conf;
+              type = with types; nullOr path;
+              description = ''
+                The fallback configuration to use at /etc/maomao/config.conf
+                '';
+            };
+
           };
+        };
 
+        config = mkIf cfg.enable {
+          environment = {
+            etc."maomao/config.conf".source = cfg.configFile;
+            systemPackages = [ self.packages.${system}.default ];
+          };
         };
       };
-
-      config = mkIf cfg.enable {
-        environment = {
-          etc."maomao/config.conf".source = cfg.configFile;
-          systemPackages = [ self.packages.x86_64-linux.default ];
-        };
-      };
-
-    }
-    ;
-  };
+    };
+  });
 }
 
 
